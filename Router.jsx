b448848c3d4c81c02314d6c1d4db0b32273af0e9
Huzaif Ahmed <dev.huzaif@gmail.com>
2025-02-12 02:23:40 +0530

    sixth-commit

--- Router.jsx
+++ Router.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './src/pages/home/Home';
 import Footer from './src/components/Footer/Footer';
+import Navbar from './src/components/Navbar/Navbar';
 
 const Router = () => {
   return (
@@ -9,7 +10,7 @@ const Router = () => {
       <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/company" element={<Company />} />
+        <Route path="/company" element={<CompanyHead/>} />
       </Routes>
       <Footer/>
     </>
