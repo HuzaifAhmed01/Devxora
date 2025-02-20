e637b7f2827d1dc15ea7eb3b01d9c2ff134ffab6
Huzaif Ahmed <dev.huzaif@gmail.com>
2025-02-20 12:40:37 +0530

    tenth-commit

--- Router.jsx
+++ Router.jsx
@@ -2,18 +2,17 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './src/pages/home/Home';
 import Footer from './src/components/Footer/Footer';
-import Navbar from './src/components/Navbar/Navbar';
-import CompanyHead from './src/components/Company/CompanyHead';
 import Projects from './src/pages/projects/Projects';
 import Privacy from './src/components/privacyPolicy/Privacy';
 import Marquee from './src/components/maquee/Marquee';
 import CompanyPage from './src/pages/company/CompanyPage';
 import Contact from './src/pages/contact/Contact';
+import Navbar from './src/components/Navbar/Navbar';
 
 const Router = () => {
   return (
     <>
-      <Navbar />
+      <Navbar/>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/privacy-policy" element={<Privacy/>}/>
@@ -22,7 +21,6 @@ const Router = () => {
         <Route path="/let's-talk" element={<Contact/>}/>
       </Routes>
       <Marquee text={'Flawless Dev, Smooth UX '} speed={15}/>
-
       <Footer/>
     </>
   );
